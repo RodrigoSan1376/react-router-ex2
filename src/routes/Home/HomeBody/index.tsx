5b19f306f8e5f623794b852291fecd24c5bcb033
Rodrigo Santana Rodrigues <rodrigosantos1376@gmail.com>
2023-04-07 15:42:13 -0300

    Project created with layout already done

---
+++ src/routes/Home/HomeBody/index.tsx
@@ -0,0 +1,19 @@
+import Button from "../../../components/Button";
+import Card from "../../../components/Card";
+
+export default function HomeBody() {
+  return (
+    <main>
+      <section>
+        <div className="container">
+          <div className="mb30 mt30">
+            <Card title="Página inicial" />
+          </div>
+          <div className="dflex">
+            <Button text="Ver promoção" />
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+}
